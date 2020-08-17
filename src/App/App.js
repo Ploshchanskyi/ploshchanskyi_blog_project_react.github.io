68bfe15d2612542beaf4687f05df9a38c044c6b9
Volodymyr <snu-snu@ukr.net>
2020-08-17 13:34:19 +0300

    created react project, added App component, base.css, reset.css, created folders Header, Main, Footer

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './../common/style/reset.css';
+import './../common/style/base.css';
+
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
